07646d889460372817c89d9bd28ebaafb8af04fd
Piyush <bhalalapj@gmail.com>
2018-02-19 14:48:09 +0530

    HelloWorld Initial Commit.

---
+++ HelloWorldRN/HelloWorld/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>Hello World!!!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle:
+  {
+    backgroundColor:'black',
+    fontSize: 15,
+  },
+});
